6a9e6847f18d0c404aec604116ae6e6a874a835d
squalxxx <squaldzn@ya.ru>
2023-09-13 15:09:40 +0300

    Made laba_3

---
+++ laba_3/52/script.js
@@ -0,0 +1,10 @@
+let str = 'abcde';
+alert(str[0], str[2], str[4]);
+
+let str2 = 'abcde';
+alert(str[4], str[3], str[2], str[1], str[0]);
+
+let str3 = 'abcde';
+let num = 3; // номер символа в переменной
+
+alert(str[num]); // выведет 'd'
---
+++ laba_3/54/script.js
@@ -0,0 +1,11 @@
+let str = 'abcde';
+let last = str.length - 1;
+alert(str[last]); // выведет 'e'
+
+let str2 = 'abcde';
+let last2 = str.length - 2;
+alert(str[last2]); // выведет 'd'
+
+let str3 = 'abcde';
+let last3 = str.length - 3;
+alert(str[last3]); // выведет 'c'
